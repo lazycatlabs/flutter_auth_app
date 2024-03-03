acb5540e43fd9c0db367800117c4e35b5ea22e86
Mudassir <hey.mudassir@gmail.com>
2024-03-03 22:23:44 +0800

    feat: update gradle config (#25)
    * chore: update library version
    
    * wip: update gradle config
    
    * feat: update gradle config
    
    * feat: update firebase config using FlutterFire CLI
    
    * chore: remove firebase messaging since we are not use it
    
    * feat: improve implementation FlutterFire CLI for multi env

--- lib/utils/services/firebase/firebase.dart
+++ lib/utils/services/firebase/firebase.dart
@@ -1,2 +1,3 @@
 export 'firebase_crashlogger.dart';
+export 'firebase_options.dart';
 export 'firebase_services.dart';
--- lib/utils/services/firebase/firebase_services.dart
+++ lib/utils/services/firebase/firebase_services.dart
@@ -2,11 +2,14 @@ import 'dart:isolate';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter_auth_app/utils/utils.dart';
 
 mixin FirebaseServices {
   static Future<void> init() async {
     /// Initialize Firebase
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
     // Pass all uncaught errors from the framework to Crashlytics.
     // FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
     await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);

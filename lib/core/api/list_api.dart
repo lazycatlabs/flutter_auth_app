e447a45d376aca32a13d8631f298362b5a37cbef
Lzyct <hey.mudassir@gmail.com>
2024-09-25 21:28:48 +0800

    chore: update base url

--- lib/core/api/list_api.dart
+++ lib/core/api/list_api.dart
@@ -1,8 +1,7 @@
 class ListAPI {
   ListAPI._();
 
-  static const String register = "/api/register";
-
-  static const String login = "/api/login";
-  static const String users = "/api/users";
+  static const String register = "/v1/api/user";
+  static const String login = "/v1/api/auth/login";
+  static const String users = "/v1/api/user/all";
 }

e06cc02d4d4aba78895820da448a09b88aceffbc
Lzyct <hey.mudassir@gmail.com>
2024-10-08 01:15:45 +0800

    clean: remove unused file

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,6 +1,5 @@
 export 'api/api.dart';
 export 'app_route.dart';
-export 'core_mapper.dart';
 export 'error/error.dart';
 export 'localization/localization.dart';
 export 'resources/resources.dart';

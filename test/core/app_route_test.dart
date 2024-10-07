9dcf69b910cc2a28f42c1cc7aa10944471f3810f
Lzyct <hey.mudassir@gmail.com>
2024-10-08 00:34:53 +0800

    wip(test): increase test coverage - 3

--- test/core/app_route_test.dart
+++ test/core/app_route_test.dart
@@ -1,19 +1,12 @@
-import 'dart:io';
-
-import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_auth_app/core/core.dart';
 import 'package:flutter_auth_app/dependencies_injection.dart';
 import 'package:flutter_auth_app/features/general/general.dart';
 import 'package:flutter_auth_app/features/users/users.dart';
-import 'package:flutter_auth_app/utils/utils.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router/go_router.dart';
-
-/// ignore: depend_on_referenced_packages
-import 'package:mocktail/mocktail.dart';
 
 /// ignore: depend_on_referenced_packages
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
@@ -21,38 +14,11 @@ import 'package:path_provider_platform_interface/path_provider_platform_interfac
 import '../helpers/fake_path_provider_platform.dart';
 import '../helpers/test_mock.mocks.dart';
 
-class MockMainCubit extends MockCubit<MainState> implements MainCubit {}
-
-class FakeMainCubit extends Fake implements MainCubit {}
-
-class MockUserCubit extends MockCubit<UserState> implements UserCubit {}
-
-class FakeUserCubit extends Fake implements UserCubit {}
-
-class MockLogoutCubit extends MockCubit<LogoutState> implements LogoutCubit {}
-
-class FakeLogoutCubit extends Fake implements LogoutCubit {}
-
 void main() {
-  late MainCubit mainCubit;
-  late UserCubit userCubit;
-  late LogoutCubit logoutCubit;
-
-  setUpAll(() {
-    HttpOverrides.global = null;
-    registerFallbackValue(FakeMainCubit());
-    registerFallbackValue(FakeUserCubit());
-    registerFallbackValue(FakeLogoutCubit());
-  });
-
   setUp(() async {
     TestWidgetsFlutterBinding.ensureInitialized();
     PathProviderPlatform.instance = FakePathProvider();
     await serviceLocator(isUnitTest: true, prefixBox: 'app_route_test_');
-
-    mainCubit = MockMainCubit();
-    userCubit = MockUserCubit();
-    logoutCubit = MockLogoutCubit();
   });
 
   test('Routes enum contains correct paths', () {
@@ -88,20 +54,6 @@ void main() {
       (WidgetTester tester) async {
     final context = MockBuildContext();
     AppRoute.setStream(context, isTest: true);
-    when(() => mainCubit.state).thenReturn(
-      MainState.success(
-        DataHelper(
-          title: Strings.of(context)?.settings ?? "Settings",
-        ),
-      ),
-    );
-    when(() => mainCubit.initMenu(context)).thenAnswer((_) async {});
-
-    when(() => userCubit.state).thenReturn(const UserState.success(null));
-    when(() => userCubit.getUser()).thenAnswer((_) async {});
-
-    when(() => logoutCubit.state).thenReturn(const LogoutState.loading());
-
     // Create a test app with the router
     await tester.pumpWidget(
       ScreenUtilInit(

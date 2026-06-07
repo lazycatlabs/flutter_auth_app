c3d2a7640597a7b5fe395b6f9f0e3f32e9455f21
Lzyct <hey.mudassir@gmail.com>
2026-06-07 14:37:09 +0800

    chore: update dependencies in pubspec.yaml

--- test/features/users/pages/dashboard/cubit/users_cubit_test.mocks.dart
+++ test/features/users/pages/dashboard/cubit/users_cubit_test.mocks.dart
@@ -23,6 +23,7 @@ import 'package:mockito/mockito.dart' as _i1;
 // ignore_for_file: unnecessary_parenthesis
 // ignore_for_file: camel_case_types
 // ignore_for_file: subtype_of_sealed_class
+// ignore_for_file: invalid_use_of_internal_member
 
 class _FakeEither_0<L, R> extends _i1.SmartFake implements _i2.Either<L, R> {
   _FakeEither_0(Object parent, Invocation parentInvocation)

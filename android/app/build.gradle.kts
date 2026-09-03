d43764f47632f71c7225fa146456c809e1a0339f
Lzyct <hey.mudassir@gmail.com>
2026-09-03 08:57:14 +0800

    chore: migrate build configuration to Kotlin DSL and update dependencies

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    id("com.android.application")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+}
+
+android {
+    namespace = "com.lazycatlabs.auth"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.lazycatlabs.auth"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+        multiDexEnabled = true
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+
+            // Enables code shrinking, obfuscation, and optimization for only
+            // your project"s release build type.
+            isMinifyEnabled = true
+
+            // Enables resource shrinking, which is performed by the
+            // Android Gradle plugin.
+            isShrinkResources = true
+
+            // Includes the default ProGuard rules files that are packaged with
+            // the Android Gradle plugin. To learn more, go to the section about
+            // R8 configuration files.
+            proguardFiles(
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    flavorDimensions += "env"
+    productFlavors {
+        create("stg") {
+            dimension = "env"
+            versionNameSuffix = "-stg"
+            applicationIdSuffix = ".stg"
+        }
+        create("prd") {
+            dimension = "env"
+        }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+
+
+//dependencies {
+//    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.0")
+//    implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
+//    implementation("androidx.multidex:multidex:2.0.1")
+//    implementation("com.google.firebase:firebase-analytics")
+//}
